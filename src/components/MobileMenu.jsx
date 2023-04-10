7e4d08bc6e6de0cf001d9382642392aed16e3073
Omar Jabraoui <omar241101skate@gmail.com>
2023-04-10 13:15:18 +0200

    Hero Section Mobile, Navbar Mobile, Services Mobile

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const MobileMenu = ({ hideMobile }) => {
     return (
-        <div className="fixed h-screen w-screen top-0 left-0 bg-slate-100 p-4">
+        <div className="fixed h-screen w-screen top-0 left-0 bg-white p-6 z-50">
             <div className="flex flex-row justify-between">
-                <a href="/" class="text-black font-semibold text-lg">omar</a>
-                <button onClick={() => hideMobile(false)}>X</button>
+                <a href="/" className="text-black font-semibold text-lg">omar</a>
+                <button onClick={() => hideMobile(false)} className='text-2xl font-semibold'>&times;</button>
             </div>
-            <ul class="flex flex-col gap-4 justify-center items-center mt-52">
-                <li><a href="/services">Servizi</a></li>
-                <li><a href="/blog">Blog</a></li>
-                <li><a href="/projects">Progetti</a></li>
-                <li><a href="/about">Chi Sono</a></li>
+            <ul class="flex flex-col gap-6 justify-center items-center mt-52">
+                <li><a href="/services" className='text-2xl font-semibold'>Servizi</a></li>
+                <li><a href="/blog" className='text-2xl font-semibold'>Blog</a></li>
+                <li><a href="/projects" className='text-2xl font-semibold'>Progetti</a></li>
+                <li><a href="/about" className='text-2xl font-semibold'>Chi Sono</a></li>
             </ul>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     const [showMobile, setShowMobile] = useState(false)
 
     return (
-        <nav class="flex flex-row items-center w-full justify-between">
-            <a href="/" class="text-black font-semibold text-lg">omar</a>
-            <ul class="hidden flex-row gap-4 md:flex">
+        <nav className="flex flex-row items-center w-full justify-between container mx-auto p-6 max-w-7xl">
+            <a href="/" className="text-black font-semibold text-lg">omar</a>
+            <ul className="hidden flex-row gap-4 md:flex">
                 <li><a href="/services">Servizi</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/projects">Progetti</a></li>
@@ -16,10 +16,10 @@ const Navbar = () => {
             </ul>
 
             {/* <!-- Hamburger Menu --> */}
-            <button class="flex flex-col justify-between gap-[0.2rem] cursor-pointer md-hidden" id="hamburger-menu" onClick={() => setShowMobile(true)}>
-                <div class="bar-1 w-5 h-[2px] bg-black"></div>
-                <div class="bar-2 w-5 h-[2px] bg-black"></div>
-                <div class="bar-3 w-5 h-[2px] bg-black"></div>
+            <button className="flex flex-col justify-between gap-[0.2rem] cursor-pointer md-hidden" id="hamburger-menu" onClick={() => setShowMobile(true)}>
+                <div className="bar-1 w-5 h-[2px] bg-black"></div>
+                <div className="bar-2 w-5 h-[2px] bg-black"></div>
+                <div className="bar-3 w-5 h-[2px] bg-black"></div>
             </button>
 
             {
